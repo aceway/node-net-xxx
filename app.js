821b236417aa45ceea643b1c432791758c70afee
aceway <aceway@qq.com>
2016-08-31 21:15:20 +0800

    rename App 2 XXX

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 #!/usr/bin/env node
 'use strict';
 var net = require('net');
+//var async = require('async');
 var logger = require('./utils/logger.js');
 
-var App = function() {
-	this.binder = require('./binder.js');
+var XXX = function(bindCfg) {
+  logger.trace("XXX(" + bindCfg+")");
+	this.binder = require(bindCfg);
 };
 
-App.prototype.start = function( ){
-  logger.trace("trace ...");    
-  logger.debug("debug ...");    
-  logger.info("info ...");    
-  logger.warn("warn ...");    
-  logger.error("error ...");    
-  logger.fatal("over ...");    
-  logger.trace("end ...");    
+XXX.prototype.start = function( ){
+  logger.trace("start( ...)");
+	var self = this;
 };
 
-var app = new App();
-app.start();
+XXX.prototype.startListen4Input = function( callback ){
+  logger.trace("startListen4Input(...)");
+}
+
+XXX.prototype.startListen4OutputHere = function( callback ){
+  logger.trace("startListen4OutputHere (...)");
+}
+
+XXX.prototype.startConnect4OutputThere = function( callback ){
+  logger.trace("startConnect4OutputThere (...)");
+}
+
+
+var xxx = new XXX('./binder.js');
+xxx.start();
 
 process.on('uncaughtException', function (err) {
   var colors = require('colors');
