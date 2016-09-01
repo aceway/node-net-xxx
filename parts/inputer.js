7dbb183d986bfa34a21fbe4fed6e020e752f669c
aceway <aceway@qq.com>
2016-09-01 21:45:23 +0800

    refine parts

--- parts/inputer.js
+++ parts/inputer.js
@@ -1,1 +1,7 @@
 'use strict';
+var logger = require('../utils/logger.js');
+
+var Inputter = function(schema, host, port) {
+};
+
+module.exports = Inputter;
--- parts/outputer.js
+++ parts/outputer.js
@@ -1,1 +1,7 @@
 'use strict';
+var logger = require('../utils/logger.js');
+
+var Outputter = function(schema, host, port, mode) {
+};
+
+module.exports = Outputter;
