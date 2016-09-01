4b9eb89842b22bb300a1a7318546813e7d29204f
aceway <aceway@qq.com>
2016-09-01 21:52:54 +0800

    x

--- starter.js
+++ starter.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
-var logger = require('./utils/logger.js');
+var colors = require('colors');
 var xxx = require('./xxx.js');
 
 var x = new xxx('./utils/binder.js');
 x.start(function(e, r){
-	logger.trace(r);
+	if ( e ){
+		console.log( JSON.stringify(r).red );
+	}
+	else{
+		console.log( JSON.stringify(r).green );
+	}
 });
 
 process.on('uncaughtException', function (err) {
-  var colors = require('colors');
   var msg = ' Caught exception: ' + err.stack;
-  logger.error(msg.red);
+  console.error(msg.red);
 });
 
