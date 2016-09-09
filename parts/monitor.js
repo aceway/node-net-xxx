6cde8bd749ace7bda2aba5b1388f8ecbce5c2560
aceway <aceway@qq.com>
2016-09-09 12:47:06 +0800

    refine tabwidth shiftwidth

--- parts/monitor.js
+++ parts/monitor.js
@@ -2,37 +2,37 @@
 var logger = require('../utils/logger.js');
 
 var Monitor = function(schema, host, port) {
-	this.schema = schema;
-	this.host = host;
-	this.port = port;
+  this.schema = schema;
+  this.host = host;
+  this.port = port;
 };
 
 Monitor.prototype.start = function (callback) {
-	var self = this;
-	//logger.trace("Monitor listen on " + self.schema  + "://" + 
-	//						self.host + ":" + self.port);
-	switch(self.schema){
-	case 'http':
-		callback(null, self.host +":"+ self.port 
-							+ " would implement schema:" + self.schema);
-		break;
-	case 'https':
-		callback(1, self.host +":"+ self.port 
-							+ " would implement schema:" + self.schema);
-		break;
-	case 'websocket':
-		callback(1, self.host +":"+ self.port 
-							+ " would implement schema:" + self.schema);
-		break;
-	case 'tcp':
-		callback(null, self.host +":"+ self.port 
-							+ " would implement schema:" + self.schema);
-		break;
-	default:
-		callback(-1, self.host +":"+ self.port 
-							+ " not surpported schema:" + self.schema);
-		break;
-	}
+  var self = this;
+  //logger.trace("Monitor listen on " + self.schema  + "://" + 
+  //            self.host + ":" + self.port);
+  switch(self.schema){
+  case 'http':
+    callback(null, self.host +":"+ self.port  +
+              " would implement schema:" + self.schema);
+    break;
+  case 'https':
+    callback(1, self.host +":"+ self.port  +
+              " would implement schema:" + self.schema);
+    break;
+  case 'websocket':
+    callback(1, self.host +":"+ self.port  +
+              " would implement schema:" + self.schema);
+    break;
+  case 'tcp':
+    callback(null, self.host +":"+ self.port + 
+              " would implement schema:" + self.schema);
+    break;
+  default:
+    callback(-1, self.host +":"+ self.port  +
+              " not surpported schema:" + self.schema);
+    break;
+  }
 };
 
 module.exports = Monitor;
