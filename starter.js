3d9b7b19a6a12626698a77e97613b4b63b25d5e8
aceway <aceway@qq.com>
2017-06-20 22:30:16 +0800

    refine Logger.js

--- starter.js
+++ starter.js
@@ -5,18 +5,17 @@ const xxx = require('./xxx.js');
 
 logger.create('./config/log4js.json', function(e, r){
   if (e){
-    console.error("Logger error, could not start server: " + e + "," + r);
+    console.error("Logger error, could not start server: " + 
+                  e + ( r ? ","+r : ""));
   }
   else{
-    //let x = new xxx();
-    //let x = new xxx('./config/bind.json');
     let x = new xxx('./config/bind.json');
     x.start(function(error, result){
       if ( error ){
-        logger.error( JSON.stringify(result).red );
+        logger.error(error + JSON.stringify(result));
       }
       else{
-        logger.info( JSON.stringify(result).green );
+        logger.info(JSON.stringify(result));
       }
     });
   }
@@ -24,7 +23,6 @@ logger.create('./config/log4js.json', function(e, r){
 
 process.on('uncaughtException', function (error) {
   let logger = require('./utils/logger.js');
-  let msg = ' Caught exception: ' + error.stack;
-  logger.error(msg.red);
+  logger.error(' Caught exception: ' + error + ',' + error.stack);
 });
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,7 @@
 'use strict';
-var fs = require("fs");
-var path = require('path');
-//var colors = require('colors');
-var log4js = require('log4js');
+const fs = require("fs");
+const path = require('path');
+const log4js = require('log4js');
 
 Date.prototype.format = function(fmt){
   if (typeof fmt !== 'string'  || fmt.length < 2) {
@@ -17,19 +16,23 @@ Date.prototype.format = function(fmt){
     "q+" : Math.floor((this.getMonth()+3)/3), //quarter
     "S" : this.getMilliseconds() //millisecond
   };
-  if(/(y+)/.test(fmt)) 
+  if(/(y+)/.test(fmt)) {
     fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - 
                     RegExp.$1.length));
-  for(let k in o)
-    if(new RegExp("("+ k +")").test(fmt))
+  }
+  for(let k in o){
+    if(new RegExp("("+ k +")").test(fmt)){
       fmt = fmt.replace(RegExp.$1, RegExp.$1.length===1 ? 
                         o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+    }
+  }
   return fmt;
 };
 let Logger = {
   option:{}, 
   pid: process.pid,
-  log_dir:"", 
+  log_path:null, 
+  log_dir:null, 
   escape_trace: false,
   escape_debug: false,
   escape_info:  false,
@@ -56,8 +59,9 @@ Logger.create = function(logCfg, callback) {
     //process.exit(1);
     return callback(-1, tip);
   }
-  let self = this;
-  console.info("[pid:"+self.pid+"]"+"Going to start server with log config: " + logCfg);
+  let self = Logger;
+  self.log_path = process.cwd() + "/logs/";
+  console.info("[pid:"+self.pid+"]"+"GOING TO START SERVER WITH LOG CONFIG: " + logCfg);
   self._prepare(function(error, result){
     self.option = require(logCfg);
     self.init(function(err, res){
@@ -71,40 +75,31 @@ Logger._prepare = function(callback) {
   try{
     let dir = new Date().format("yyyy-MM-dd");
     if (dir === self.log_dir){
-      return callback(null, "Logger._prepare OK");
+      return callback(null, "Logger._prepare prev OK");
     }
-
-    let fullPath = process.cwd() + "/logs/";
-    if( !fs.existsSync(fullPath) ){
-      //console.log("Make the new dir:".red + fullPath);
-      fs.mkdirSync(fullPath);
-      fullPath = process.cwd() + "/logs/" + dir;
-      if( !fs.existsSync(fullPath) ){
-        //console.log("Make the new dir:".red + fullPath);
-        fs.mkdirSync(fullPath);
-      }
-      else{
-        //console.log("The dir has exist:".green + fullPath);
+    let full_path = self.log_path + dir;
+    if( !fs.existsSync(self.log_path) ){
+      fs.mkdirSync(self.log_path);
+      full_path = self.log_path + dir;
+      if( !fs.existsSync(full_path) ){
+        fs.mkdirSync(full_path);
       }
       self.log_dir = dir;
     }
     else{
-      //console.log("The dir has exist:".green + fullPath);
-      fullPath = process.cwd() + "/logs/" + dir;
-      if( !fs.existsSync(fullPath) ){
-        //console.log("Make the new dir:".red + fullPath);
-        fs.mkdirSync(fullPath);
-      }
-      else{
-        //console.log("The dir has exist:".green + fullPath);
+      full_path = self.log_path + dir;
+      if( !fs.existsSync(full_path) ){
+        fs.mkdirSync(full_path);
       }
       self.log_dir = dir;
     }
+    return callback(null, "Logger._prepare OK. new date dir.");
   }
   catch(e){
-    console.warn("Logger._prepare throw exception:" + e);
+    let tips = "Logger._prepare throw exception:" + e;
+    console.warn( tips + "," + e.stack);
+    return callback(-1, tips);
   }
-  callback(null, "Logger._prepare OK. new date dir.");
 };
 
 Logger.init = function (callback) {
@@ -112,7 +107,6 @@ Logger.init = function (callback) {
   log4js.configure(self.option);
 
   
-
   // 终端调试输出
   self.logConsole = log4js.getLogger('console');
 
@@ -152,7 +146,7 @@ Logger.init = function (callback) {
     //console.error(log4js.levels.getLevel("ERROR").level);
     //console.error(log4js.levels.getLevel("FATAL").level);
     if (cfg && lvl){
-      console.info("Got cfg logger level: " + cfg + " - " + lvl);
+      console.info("GOT CFG LOGGER LEVEL: " + cfg + " - " + lvl);
       if ( lvl === log4js.levels.getLevel("ALL").level){
         self.escape_trace = false;
         self.escape_debug = false;
@@ -213,7 +207,7 @@ Logger.init = function (callback) {
   }
   else if (self.option && self.option.levels && self.option.levels["[all]"]){
     let lvl = self.option.levels["[all]"];
-    console.info("Got cfg logger level: " + lvl );
+    console.info("GOT CFG LOGGER LEVEL: " + lvl );
     switch (lvl){
       case "ALL":
         self.escape_trace = false;
@@ -274,7 +268,7 @@ Logger.init = function (callback) {
     }
   }
   else{
-    console.info("Use default logger level: " + lvl );
+    console.info("Use default logger level: INFO!");
     self.escape_trace = true;
     self.escape_debug = true;
     self.escape_info  = false;
@@ -283,6 +277,8 @@ Logger.init = function (callback) {
     self.escape_fatal = false;
   }
 
+  self.info("[PID:" + process.pid +
+                 "] MAKE LOGS AT PATH: " + self.log_path);
   callback(null, "Logger.init OK");
 };
 
@@ -301,11 +297,15 @@ Logger.console = function (msg) {
 
 Logger.trace = function (msg) {
   let self = Logger;
-  if (self.escape_trace) return;
+  if (self.escape_trace) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.trace(msg);
-    if (self.logAll_ && self.logAll_.trace) self.logAll_.trace(msg);
+    if (self.logAll_ && self.logAll_.trace) {
+      self.logAll_.trace(msg);
+    }
 
     if (typeof self.logTrace === 'undefined') {
       console.log(msg);
@@ -318,11 +318,15 @@ Logger.trace = function (msg) {
 
 Logger.debug = function (msg) {
   let self = Logger;
-  if (self.escape_debug) return;
+  if (self.escape_debug) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.debug(msg);
-    if (self.logAll_ && self.logAll_.debug) self.logAll_.debug(msg);
+    if (self.logAll_ && self.logAll_.debug) {
+      self.logAll_.debug(msg);
+    }
 
     if (typeof self.logDebug === 'undefined') {
       console.log(msg);
@@ -335,11 +339,15 @@ Logger.debug = function (msg) {
 
 Logger.info = function (msg) {
   let self = Logger;
-  if (self.escape_info) return;
+  if (self.escape_info) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.info(msg);
-    if (self.logAll_ && self.logAll_.info) self.logAll_.info(msg);
+    if (self.logAll_ && self.logAll_.info) {
+      self.logAll_.info(msg);
+    }
 
     if (typeof self.logInfo === 'undefined') {
       console.info(msg);
@@ -352,11 +360,15 @@ Logger.info = function (msg) {
 
 Logger.warn = function (msg) {
   let self = Logger;
-  if (self.escape_warn) return;
+  if (self.escape_warn) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.warn(msg);
-    if (self.logAll_ && self.logAll_.warn) self.logAll_.warn(msg);
+    if (self.logAll_ && self.logAll_.warn) {
+      self.logAll_.warn(msg);
+    }
 
     if (typeof self.logWarn === 'undefined') {
       console.warn(msg);
@@ -369,11 +381,15 @@ Logger.warn = function (msg) {
 
 Logger.error = function (msg) {
   let self = Logger;
-  if (self.escape_error) return;
+  if (self.escape_error) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.error("[pid:"+self.pid+"]"+msg);
-    if (self.logAll_ && self.logAll_.error) self.logAll_.error("[pid:"+self.pid+"]"+msg);
+    if (self.logAll_ && self.logAll_.error) {
+      self.logAll_.error("[pid:"+self.pid+"]"+msg);
+    }
 
     if (typeof self.logError === 'undefined') {
       console.error("[pid:"+self.pid+"]"+msg);
@@ -386,11 +402,15 @@ Logger.error = function (msg) {
 
 Logger.fatal = function (msg) {
   let self = Logger;
-  if (self.escape_fatal) return;
+  if (self.escape_fatal) {
+    return;
+  }
 
   self._prepare(function(){
     self.logConsole.fatal("[pid:"+self.pid+"]"+msg);
-    if (self.logAll_ && self.logAll_.fatal) self.logAll_.fatal("[pid:"+self.pid+"]"+msg);
+    if (self.logAll_ && self.logAll_.fatal) {
+      self.logAll_.fatal("[pid:"+self.pid+"]"+msg);
+    }
 
     if (typeof self.logFatal === 'undefined') {
       console.error("[pid:"+self.pid+"]"+msg);
